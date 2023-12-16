5f7967ca7b71b84e13bff77f759bf27ccecf1e8d
Park JunYong <dyden119@gmail.com>
2023-12-17 02:06:16 +0900

    feat: 주어진 이메일,패스워드로 로그인시 folder페이지로 이동

--- signin/common.js
+++ signin/common.js
@@ -1,6 +1,8 @@
 export const emailInput = document.querySelector('#inputEmail');
+export const passwordInput = document.querySelector('#inputPassword');
 export const errMsg = document.createElement('h6');
 export const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+export const submit = document.querySelector('.submit');
 
 
 
--- signin/script.js
+++ signin/script.js
@@ -1,3 +1,12 @@
 import * as common from "./common.js";
 
 common.emailInput.addEventListener('focusout', common.isEmailValid);
+
+common.submit.addEventListener('click', (e)=>{
+  e.preventDefault();
+  if(common.emailInput.value=='test@codeit.com'&&common.passwordInput.value=='codeit101'){
+    location.href='../folder/folder.html'
+  }else{
+    alert('동작확인용')
+  }
+})
